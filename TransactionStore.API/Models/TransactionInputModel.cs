b25d18851d4f3a482ce63ebbc8fc29f986a6c21d
Семен Яковлев <semion.iakowlev@yandex.ru>
2022-03-05 16:43:31 +0300

    change TransactionType

--- TransactionStore.API/Models/TransactionInputModel.cs
+++ TransactionStore.API/Models/TransactionInputModel.cs
@@ -5,7 +5,7 @@ namespace TransactionStore.API.Models
     public class TransactionInputModel
     {
         public DateTime Date { get; set; }
-        public TypeTransaction Type { get; set; }
+        public TransactionType Type { get; set; }
         public decimal Amount { get; set; }
         public int AccountId { get; set; }
     }
