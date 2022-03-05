319bdfe7d247773a888dc1bd2e3d69d88e76a23a
Семен Яковлев <semion.iakowlev@yandex.ru>
2022-03-05 17:05:35 +0300

    delete Date and Type fram TransactionRequestModel

--- TransactionStore.API/Models/Request/TransactionRequestModel.cs
+++ TransactionStore.API/Models/Request/TransactionRequestModel.cs
@@ -4,8 +4,6 @@ namespace TransactionStore.API.Models
 {
     public class TransactionRequestModel
     {
-        public DateTime Date { get; set; }
-        public TransactionType Type { get; set; }
         public decimal Amount { get; set; }
         public int AccountId { get; set; }
     }
