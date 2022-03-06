664a0f047cdf7fef4f7ad230cb3ba140201aee94
Семен Яковлев <semion.iakowlev@yandex.ru>
2022-03-06 11:52:35 +0300

    first step to add connection string

---
+++ TransactionStore.API/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TransactionStore.BusinessLayer.Services;
+using TransactionStore.BusinessLayer.Services.Interfaces;
+using TransactionStore.DataLayer.Repository;
+
+namespace TransactionStore.API
+{
+    public static class ServiceProviderExtensions
+    {
+        public static void RegisterTransactionStoreServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITransactionService, TransactionService>();
+        }
+
+        public static void RegisterTransactionStoreRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ITransactionRepository, TransactionRepository>();
+        }
+    }
+}
--- TransactionStore.API/Program.cs
+++ TransactionStore.API/Program.cs
@@ -1,23 +1,22 @@
+using TransactionStore.API;
 using TransactionStore.API.Configuration;
 using TransactionStore.BuisnessLayer.Configuration;
-using TransactionStore.BusinessLayer.Services;
-using TransactionStore.BusinessLayer.Services.Interfaces;
-using TransactionStore.DataLayer.Repository;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddConnectionString();
+
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(typeof(BuisnessMapper).Assembly, typeof(DataMapper).Assembly);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped<ITransactionService, TransactionService>();
-builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
 builder.Services.AddSwaggerGen(c => { c.EnableAnnotations(); });
+
+builder.Services.RegisterTransactionStoreServices();
+builder.Services.RegisterTransactionStoreRepositories();
 
 var app = builder.Build();
 
