d336f085e6615991792b3de9025652bb9d3b7b0c
saenkaaa <saenkaaa.riki@gmail.com>
2022-03-07 14:29:17 +0300

    Add store procedure for getByAccountId

---
+++ TransactionStore.DB/Procedures/Transaction_SelectByAccountId.sql
@@ -0,0 +1,15 @@
+﻿CREATE proc [dbo].[Transaction_SelectByAccountId]
+			@AccountId int
+AS
+BEGIN
+SELECT
+		Id,
+		Amount,
+		Date,
+		Type, 
+		AccountId,
+		Currency
+		
+	from dbo.[Transaction]
+	where AccountId = @AccountId						
+END
