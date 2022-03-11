dad180945cf20f3b80646924bbd1d18b69fede79
saenkaaa <saenkaaa.riki@gmail.com>
2022-03-11 17:32:40 +0300

    edit procedure again

--- TransactionStore.DB/Procedures/Transaction_SelectByAccountId.sql
+++ TransactionStore.DB/Procedures/Transaction_SelectByAccountId.sql
@@ -3,19 +3,26 @@
 AS
 BEGIN
 SELECT
-		t.Id,
-		t.Amount,
-		t.Date ,
-		t.Type, 
-		t.AccountId,
-		t.Currency,
+		Id,
+		Amount,
+		Date ,
+		Type, 
+		AccountId,
+		Currency
+		
+	from dbo.[Transaction]	
+	where AccountId = @AccountId 
+	union all
+SELECT
 		r.Id,
-		r.Date,
-		r.AccountId	
+		r.Amount,
+		r.Date ,
+		r.Type, 
+		r.AccountId,
+		r.Currency
 		
 	from dbo.[Transaction] t left JOIN [Transaction] r on r.Date = t.Date
-	where t.AccountId = @AccountId or
-	(t.AccountId = @AccountId and t.Type = 3) and
+	where (t.AccountId = @AccountId and t.Type = 3) and
 	(r.AccountId != @AccountId and r.Type = 3);
 	
 END
